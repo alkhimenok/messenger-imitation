b90f5328735d8587f5027a4f5e92e595f6781498
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-01-26 14:03:16 +0300

    add: react app

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Body, Container } from './common/styleComponents'
+
+const Header = () => {
+	return (
+		<header className="header">
+			<Container className="header__container">
+				<Body className="header__body">
+					<h1>hello</h1>
+				</Body>
+			</Container>
+		</header>
+	)
+}
+
+export default Header
---
+++ src/components/common/styleComponents.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	max-width: 1200px;
+	margin: 0 auto;
+	background-color: red;
+`
+
+export const Body = styled.div`
+	padding: 0 5px
+`
