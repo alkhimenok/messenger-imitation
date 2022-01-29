6e8561c28e8a8c0b89c20906f51623824abf4803
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-01-29 17:27:53 +0300

    add: client scripts

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = 5000
 
 app.use(express.static(path.resolve(__dirname, 'client', 'build')))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile('/index.html'))
 
 const start = () => {
 	app.listen(PORT, () => console.log(`server started on port ${PORT}`))
